09afc3fce726ceab50f7a409941208e2619c6d0f
wdy <106950730+tonywdy@users.noreply.github.com>
2022-07-27 18:01:13 +0800

    Create Ipaddress.vue

---
+++ src/core/commands/ip/Ipaddress.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <div>Detection succeeded! Next for your IP！</div>
+    <img src="https://www.ouklc.com/api/ip">
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
