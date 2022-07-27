a109726a9f697789180f9183f976cd7ad36061d6
wdy <106950730+tonywdy@users.noreply.github.com>
2022-07-28 07:14:38 +0800

    Update YuTerminal.vue

--- src/components/yu-terminal/YuTerminal.vue
+++ src/components/yu-terminal/YuTerminal.vue
@@ -83,7 +83,7 @@
 </template>
 
 <script setup lang="ts">
-import { computed, onMounted, ref, StyleValue, toRefs, watchEffect } from "vue";
+import { computed, onMounted, Ref, ref, StyleValue, toRefs, watchEffect } from "vue";
 import CommandOutputType = YuTerminal.CommandOutputType;
 import OutputType = YuTerminal.OutputType;
 import CommandInputType = YuTerminal.CommandInputType;
@@ -316,6 +316,21 @@ const writeOutput = (newOutput: OutputType) => {
 };
 
 /**
+ * 获取输入框是否聚焦
+ */
+const isInputFocused = () => {
+  return (commandInputRef.value.input as HTMLInputElement) == document.activeElement
+}
+/**
+ * 设置输入框的值
+ */
+const setTabCompletion = () => {
+  if(hint.value){
+    inputCommand.value.text =`${hint.value.split(' ')[0]}${hint.value.split(' ').length > 1 ? ' ' : ''}`
+  }
+}
+
+/**
  * 输入框聚焦
  */
 const focusInput = () => {
@@ -349,6 +364,8 @@ const terminal: TerminalType = {
   writeOutput,
   clear,
   focusInput,
+  isInputFocused,
+  setTabCompletion,
   doSubmitCommand,
   showNextCommand,
   showPrevCommand,
