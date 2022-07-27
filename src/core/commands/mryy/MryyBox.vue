b76436b99e3c476b89c4186d197b2eefa79bb19a
wdy <106950730+tonywdy@users.noreply.github.com>
2022-07-27 09:21:10 +0800

    Create MryyBox.vue

---
+++ src/core/commands/mryy/MryyBox.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <span id="jinrishici-sentence">正在加载今日诗词....</span>
+    <script src="https://sdk.jinrishici.com/v2/browser/jinrishici.js" charset="utf-8"></script>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
