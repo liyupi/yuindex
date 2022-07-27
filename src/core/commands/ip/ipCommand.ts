a28496521148c34dd0c359dbce8dde495fed75fa
wdy <106950730+tonywdy@users.noreply.github.com>
2022-07-27 18:04:11 +0800

    Create ipCommand.ts

---
+++ src/core/commands/ip/ipCommand.ts
@@ -0,0 +1,25 @@
+import { CommandType } from "../../command";
+import { defineAsyncComponent } from "vue";
+import ComponentOutputType = YuTerminal.ComponentOutputType;
+
+/**
+ * ip查询
+ * @author kingwdy
+ */
+const ddosCommand: CommandType = {
+  func: "ip address",
+  name: "ip查询",
+  alias: ["ip"],
+  desc: "ip查询",
+  options: [],
+  action(options, terminal) {
+    const output: ComponentOutputType = {
+      type: "component",
+      component: defineAsyncComponent(() => import("./IpBox.vue")),
+      props: {},
+    };
+    terminal.writeResult(output);
+  },
+};
+
+export default ddosCommand;
