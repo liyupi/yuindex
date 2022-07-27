e96ca473abf38fe0b6e6f0a3bdd5dd8e7ba6fb45
wdy <106950730+tonywdy@users.noreply.github.com>
2022-07-27 09:29:27 +0800

    Update MryyBox.vue

--- src/core/commands/mryy/MryyBox.vue
+++ src/core/commands/mryy/MryyBox.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
-    <span id="jinrishici-sentence">正在加载今日诗词....</span>
-    <script src="https://sdk.jinrishici.com/v2/browser/jinrishici.js" charset="utf-8"></script>
+  <p>施工中</p>
   </div>
 </template>
 
