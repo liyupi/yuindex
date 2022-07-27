bf4ea8da734a87c1b2a12082b2d1d957c88fd03a
wdy <106950730+tonywdy@users.noreply.github.com>
2022-07-27 09:52:33 +0800

    Update NoweatherBox.vue

--- src/core/commands/noweather/NoweatherBox.vue
+++ src/core/commands/noweather/NoweatherBox.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
-    <p>本项目目前为bate，不是很稳定！</p>
-    <iframe width="280" height="300" frameborder="0" scrolling="no" hspace="0" src="https://i.tianqi.com/?c=code&a=getcode&id=55&icon=1" style="border:solid 1px #7ec8ea"></iframe>
+  <iframe width="280" height="300" frameborder="0" scrolling="no" hspace="0" src="https://i.tianqi.com/?c=code&a=getcode&id=55&icon=1" style="border:solid 1px #7ec8ea"></iframe>
   </div>
 </template>
 
