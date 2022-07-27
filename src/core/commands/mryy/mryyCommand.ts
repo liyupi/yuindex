5af45b683fa29d7a3c07a9dbf154597f1fc8df86
wdy <106950730+tonywdy@users.noreply.github.com>
2022-07-27 09:21:35 +0800

    Add files via upload

---
+++ src/core/commands/mryy/mryyCommand.ts
@@ -0,0 +1,24 @@
+import { CommandType } from "../../command";
+import { defineAsyncComponent } from "vue";
+import ComponentOutputType = YuTerminal.ComponentOutputType;
+
+/**
+ * 每日一言
+ * @author kingwdy
+ */
+const ddosCommand: CommandType = {
+  func: "meyy",
+  name: "每日一言",
+  desc: "每日一言",
+  options: [],
+  action(options, terminal) {
+    const output: ComponentOutputType = {
+      type: "component",
+      component: defineAsyncComponent(() => import("./MryyBox.vue")),
+      props: {},
+    };
+    terminal.writeResult(output);
+  },
+};
+
+export default ddosCommand;
