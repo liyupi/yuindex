1ba04bb47dfd121f2c01c14781aea81d160054f0
wdy <106950730+tonywdy@users.noreply.github.com>
2022-07-28 13:30:42 +0800

    Update NoteBox.vue

--- src/core/commands/note/NoteBox.vue
+++ src/core/commands/note/NoteBox.vue
@@ -1,5 +1,8 @@
 <template>
   <div>
+  <div>This project is independently developed by kingwdy.</div>
+  <div>This function is based on cookies. Please do not delete or block cookies!</div>
+  <div>Cloud synchronization will be added in the future. Wait until Yupi gets cloud synchronization out 😁</div>
   <iframe width="900" height="500" frameborder="0" hspace="0" src="https://drawing-o9y3lqglw-yqmmkingdom.vercel.app/notes/" style="border:solid 1px #7ec8ea"></iframe>
   </div>
 </template>
