378930a2b942aa5d8e46348060acd522e9d30532
wdy <106950730+tonywdy@users.noreply.github.com>
2022-07-28 12:17:39 +0800

    Create DrawBox.vue

---
+++ src/core/commands/draw/DrawBox.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <iframe src="https://cirolee.github.io/html_css_javascript/drawing/" ></iframe>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
