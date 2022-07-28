6fcbce15e1e7a652dbf887105d17385f1aac1cc3
wdy <106950730+tonywdy@users.noreply.github.com>
2022-07-28 20:17:42 +0800

    Update gengCommand.ts

--- src/core/commands/search/gengCommand.ts
+++ src/core/commands/search/gengCommand.ts
@@ -26,8 +26,8 @@ const gengCommand: CommandType = {
   ],
   action(options, terminal) {
     const { _, self } = options;
-    const word = _.length > 0 ? _[0] : "";
-    const targetLink = `https://jikipedia.com/search?phrase=${word}`;
+    const word = _.length > 0 ? _.join(' ') : '';
+    const targetLink = `https://jikipedia.com/search?phrase=${word?.trim()}`;
     if (self) {
       window.location.href = targetLink;
     } else {
