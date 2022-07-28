d7d3a1b5f0cba46595f5b105fbc657dcb1b5efa8
wdy <106950730+tonywdy@users.noreply.github.com>
2022-07-28 13:18:27 +0800

    Create NoteBox.vue

---
+++ src/core/commands/note/NoteBox.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+  <iframe width="900" height="500" frameborder="0" scrolling="no" hspace="0" src="https://drawing-o9y3lqglw-yqmmkingdom.vercel.app/notes/" style="border:solid 1px #7ec8ea"></iframe>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
