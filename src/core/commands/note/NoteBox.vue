5c70ee2ae97bfd77e34b90a3051848e79c5f84eb
wdy <106950730+tonywdy@users.noreply.github.com>
2022-07-28 13:24:51 +0800

    Update NoteBox.vue

--- src/core/commands/note/NoteBox.vue
+++ src/core/commands/note/NoteBox.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-  <iframe width="900" height="500" frameborder="0" scrolling="no" hspace="0" src="https://drawing-o9y3lqglw-yqmmkingdom.vercel.app/notes/" style="border:solid 1px #7ec8ea"></iframe>
+  <iframe width="900" height="500" frameborder="0" hspace="0" src="https://drawing-o9y3lqglw-yqmmkingdom.vercel.app/notes/" style="border:solid 1px #7ec8ea"></iframe>
   </div>
 </template>
 
