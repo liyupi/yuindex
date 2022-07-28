d3b2c870c26e9e088020ab3fc8fbc21de2a7305a
wdy <106950730+tonywdy@users.noreply.github.com>
2022-07-28 12:30:11 +0800

    Update DrawBox.vue

--- src/core/commands/draw/DrawBox.vue
+++ src/core/commands/draw/DrawBox.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <iframe src="https://drawing-o9y3lqglw-yqmmkingdom.vercel.app/drawing" width="900" height="500"></iframe>
+    <iframe src="https://drawing-o9y3lqglw-yqmmkingdom.vercel.app/drawing/" width="900" height="500"></iframe>
   </div>
 </template>
 
