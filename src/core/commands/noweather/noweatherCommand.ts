3eb58aa32e51a1592965aa105841ca5b544cbc29
wdy <106950730+tonywdy@users.noreply.github.com>
2022-07-28 13:47:36 +0800

    Update noweatherCommand.ts

--- src/core/commands/noweather/noweatherCommand.ts
+++ src/core/commands/noweather/noweatherCommand.ts
@@ -11,6 +11,7 @@ const ddosCommand: CommandType = {
   name: "noweather",
   desc: "查看现在天气",
   alias: ["nw"],
+  collapsible: true,
   options: [],
   action(options, terminal) {
     const output: ComponentOutputType = {
