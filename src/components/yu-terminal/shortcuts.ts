45192f37c2d7c87c1ee8e6f41eea1ef4c2c65004
wdy <106950730+tonywdy@users.noreply.github.com>
2022-07-29 10:50:28 +0800

    Update shortcuts.ts

--- src/components/yu-terminal/shortcuts.ts
+++ src/components/yu-terminal/shortcuts.ts
@@ -10,11 +10,15 @@ import TerminalType = YuTerminal.TerminalType;
  */
 export const registerShortcuts = (terminal: TerminalType) => {
   document.onkeydown = (e) => {
-    console.log(e);
+    // console.log(e);
     let key = e.key;
     // 自动聚焦输入框
     if (key >= "a" && key <= "z" && !e.metaKey && !e.shiftKey && !e.ctrlKey) {
-      terminal.focusInput();
+      if (terminal.isInputFocused()) {
+        terminal.setTabCompletion();
+      } else {
+        terminal.focusInput();
+      }
       return;
     }
     // 匹配快捷键
