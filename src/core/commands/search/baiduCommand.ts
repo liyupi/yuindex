64dc92d42ae9211208a05319b18ecd51fb07bf80
jimmyxuexue <1002661758@qq.com>
2022-07-29 18:30:57 +0800

    :bug: 修复search时不能有空格

--- src/core/commands/search/baiduCommand.ts
+++ src/core/commands/search/baiduCommand.ts
@@ -5,46 +5,46 @@ import { CommandType } from "../../command";
  * @author yupi
  */
 const baiduCommand: CommandType = {
-  func: "baidu",
-  name: "百度搜索",
-  alias: [],
-  params: [
-    {
-      key: "word",
-      desc: "搜索内容",
-      required: true,
-    },
-  ],
-  options: [
-    {
-      key: "self",
-      desc: "是否当前页面打开",
-      alias: ["s"],
-      type: "boolean",
-      defaultValue: false,
-    },
-    {
-      key: "picture",
-      desc: "是否搜索图片",
-      alias: ["p"],
-      type: "boolean",
-      defaultValue: false,
-    },
-  ],
-  action(options, terminal) {
-    const { _, self, picture } = options;
-    const word = _.length > 0 ? _[0] : "";
-    let targetLink = `https://www.baidu.com/s?wd=${word}`;
-    // 搜索图片
-    if (picture) {
-      targetLink = `https://image.baidu.com/search/index?tn=baiduimage&word=${word}`;
-    }
-    if (self) {
-      window.location.href = targetLink;
-    } else {
-      window.open(targetLink);
-    }
-  },
+	func: "baidu",
+	name: "百度搜索",
+	alias: [],
+	params: [
+		{
+			key: "word",
+			desc: "搜索内容",
+			required: true,
+		},
+	],
+	options: [
+		{
+			key: "self",
+			desc: "是否当前页面打开",
+			alias: ["s"],
+			type: "boolean",
+			defaultValue: false,
+		},
+		{
+			key: "picture",
+			desc: "是否搜索图片",
+			alias: ["p"],
+			type: "boolean",
+			defaultValue: false,
+		},
+	],
+	action(options, terminal) {
+		const { _, self, picture } = options;
+		const word = _.length > 0 ? _.join(" ") : "";
+		let targetLink = `https://www.baidu.com/s?wd=${word?.trim()}`;
+		// 搜索图片
+		if (picture) {
+			targetLink = `https://image.baidu.com/search/index?tn=baiduimage&word=${word}`;
+		}
+		if (self) {
+			window.location.href = targetLink;
+		} else {
+			window.open(targetLink);
+		}
+	},
 };
 
 export default baiduCommand;
