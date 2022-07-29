2129e7c111a83ea1221fd1c2e39429277f92f252
jimmyxuexue <1002661758@qq.com>
2022-07-29 15:44:28 +0800

    :fire: 格式优化调整

--- src/core/commands/fanyi/fanyiCommand.ts
+++ src/core/commands/fanyi/fanyiCommand.ts
@@ -6,51 +6,52 @@ import { translate } from "./fanYiApi";
  * @author yupi
  */
 const fanyiCommand: CommandType = {
-  func: "fanyi",
-  name: "翻译",
-  alias: [],
-  params: [
-    {
-      key: "word",
-      desc: "要翻译的内容",
-      required: true,
-    },
-  ],
-  options: [
-    {
-      key: "from",
-      desc: "源语言",
-      alias: ["f"],
-      type: "string",
-      defaultValue: "auto",
-    },
-    {
-      key: "to",
-      desc: "目标语言",
-      alias: ["t"],
-      type: "string",
-      defaultValue: "auto",
-    },
-  ],
-  async action(options, terminal) {
-    const { _, from, to } = options;
-    if (_.length < 1) {
-      terminal.writeTextErrorResult("参数不足");
-      return;
-    }
-    const keywords = _[0];
-    const res: any = await translate(keywords, {
-      from,
-      to,
-    });
-    if (res?.code === 0) {
-      terminal.writeTextSuccessResult(
-        `翻译结果：${res.data.trans_result[0].dst}`
-      );
-    } else {
-      terminal.writeTextErrorResult(res?.message ?? "翻译失败");
-    }
-  },
+	func: "fanyi",
+	name: "翻译",
+	alias: [],
+	params: [
+		{
+			key: "word",
+			desc: "要翻译的内容",
+			required: true,
+		},
+	],
+	options: [
+		{
+			key: "from",
+			desc: "源语言",
+			alias: ["f"],
+			type: "string",
+			defaultValue: "auto",
+		},
+		{
+			key: "to",
+			desc: "目标语言",
+			alias: ["t"],
+			type: "string",
+			defaultValue: "auto",
+		},
+	],
+	async action(options, terminal) {
+		const { _, from, to } = options;
+		if (_.length < 1) {
+			terminal.writeTextErrorResult("参数不足");
+			return;
+		}
+		const keywords = _[0];
+		const res: any = await translate(keywords, {
+			from,
+			to,
+		});
+		console.log(keywords, "keywords");
+		if (res?.code === 0) {
+			terminal.writeTextSuccessResult(
+				`翻译结果：${res.data.trans_result[0].dst}`
+			);
+		} else {
+			terminal.writeTextErrorResult(res?.message ?? "翻译失败");
+		}
+	},
 };
 
 export default fanyiCommand;
