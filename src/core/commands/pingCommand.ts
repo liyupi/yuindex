b44d6bc6859f328b228d1bd3234a4df71eb6c9c1
xqy2006 <48169260+xqy2006@users.noreply.github.com>
2022-07-30 19:06:28 +0800

    fix:修复并优化ping命令

--- src/core/commands/pingCommand.ts
+++ src/core/commands/pingCommand.ts
@@ -16,24 +16,47 @@ const pingCommand: CommandType = {
       required: true,
     },
   ],
-  options: [],
+  options: [
+    {
+      key: "timeout",
+      desc: "请求超时时间(单位:毫秒)",
+      alias: ["t"],
+      type: "string",
+      defaultValue: "3000",
+    },
+  ],
   async action(options, terminal) {
     const { _ } = options;
+    const { timeout = "3000" } = options;
     if (_.length < 1) {
       terminal.writeTextErrorResult("参数不足");
       return;
     }
     var dest = _[0];
     if (
-      dest.substr(0, 7).toLowerCase() != "http://" &&
-      dest.substr(0, 8).toLowerCase() != "https://"
+      !dest.toLowerCase().startsWith("http://") &&
+      !dest.toLowerCase().startsWith("https://")
     ) {
-      dest = "http://" + dest;
+      dest = "https://" + dest;
     }
-    const res = await fetch(dest, { mode: "no-cors" })
-      .then((resp) => {
+    if (dest.toLowerCase().startsWith("http://")) {
+      dest = dest.replace("http://", "https://");
+    }
+    const startTime = new Date().getTime();
+    const res = await Promise.race([
+      new Promise(function (resolve, reject) {
+        setTimeout(() => reject(new Error("timeout")), Number(timeout));
+      }),
+      fetch(dest, { mode: "no-cors" }),
+    ])
+      .then((resp: any) => {
         if (resp.ok || resp.status == 200 || resp.type == "opaque") {
+          console.log(resp);
+          const finishTime = new Date().getTime();
           terminal.writeTextSuccessResult("目标地址正常");
+          terminal.writeTextResult(
+            `延迟=${(finishTime - startTime).toString()}ms`
+          );
         } else {
           terminal.writeTextErrorResult("ping 不通！");
         }
