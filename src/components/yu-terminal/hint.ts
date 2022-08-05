fd983244e36928031ebe29e656cd058747758e6e
Amatke31 <2352595327@qq.com>
2022-08-06 01:00:43 +0800

    修复无法使用空格的问题，现在可以用双引号或单引号

--- src/components/yu-terminal/hint.ts
+++ src/components/yu-terminal/hint.ts
@@ -21,7 +21,34 @@ const useHint = () => {
       hint.value = "";
       return;
     }
-    const args = trim(inputText).split(" ");
+    const originArgs = trim(inputText).split(" ");
+    let args: string[] = []
+    // 处理空格
+    for (let index = 0; originArgs[index]; index++) {
+      const arg = originArgs[index];
+      if (arg[0] == "\"" || arg[0] == "'") {
+        let end = "";
+        const _index = index;
+        for (let i = index; originArgs[i]; i++){
+          const arg = originArgs[i];
+          if (arg[arg.length] == "\"" || arg[arg.length] == "'") {
+            end = end + (i == _index? "" : " ") + arg.slice(0, arg.length - 1);
+            break;
+          }
+          else {
+            end = end + (i == _index? "" : " ") + arg;
+          }
+          index++
+        }
+        if (end[end.length - 1] == "\"" || end[end.length - 1] == "'") {
+          end = end.substring(0, end.length - 1)
+        }
+        args.push(end.substring(1, end.length + 1));
+      }
+      else {
+        args.push(arg);
+      }
+    }
     const func = args[0];
     // 前缀匹配
     const likeKey = Object.keys(commandMap).filter((key) =>
--- src/core/commandExecutor.ts
+++ src/core/commandExecutor.ts
@@ -75,7 +75,34 @@ const doParse = (
   commandOptions: CommandOptionType[]
 ): getopts.ParsedOptions => {
   // 过滤掉关键词
-  const args: string[] = text.split(" ").slice(1);
+  const originArgs = text.split(" ").slice(1);
+  let args: string[] = []
+  // 处理空格
+  for (let index = 0; originArgs[index]; index++) {
+    const arg = originArgs[index];
+    if (arg[0] == "\"" || arg[0] == "'") {
+      let end = "";
+      const _index = index;
+      for (let i = index; originArgs[i]; i++){
+        const arg = originArgs[i];
+        if (arg[arg.length] == "\"" || arg[arg.length] == "'") {
+          end = end + (i == _index? "" : " ") + arg.slice(0, arg.length - 1);
+          break;
+        }
+        else {
+          end = end + (i == _index? "" : " ") + arg;
+        }
+        index++
+      }
+      if (end[end.length - 1] == "\"" || end[end.length - 1] == "'") {
+        end = end.substring(0, end.length - 1)
+      }
+      args.push(end.substring(1, end.length + 1));
+    }
+    else {
+      args.push(arg);
+    }
+  }
   // 转换
   const options: getopts.Options = {
     alias: {},
