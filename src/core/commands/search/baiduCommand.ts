e34b90c58980fd0ac1e9afc6e16e89e4d62ab4df
jimmyxuexue <1002661758@qq.com>
2022-08-13 09:02:58 +0800

    cc

--- src/core/commands/search/baiduCommand.ts
+++ src/core/commands/search/baiduCommand.ts
@@ -1,50 +1,50 @@
-import { CommandType } from "../../command";
+import { CommandType } from '../../command'
 
 /**
  * 百度搜索命令
  * @author yupi
  */
 const baiduCommand: CommandType = {
-	func: "baidu",
-	name: "百度搜索",
+	func: 'baidu',
+	name: '百度搜索',
 	alias: [],
 	params: [
 		{
-			key: "word",
-			desc: "搜索内容",
+			key: 'word',
+			desc: '搜索内容',
 			required: true,
 		},
 	],
 	options: [
 		{
-			key: "self",
-			desc: "是否当前页面打开",
-			alias: ["s"],
-			type: "boolean",
+			key: 'self',
+			desc: '是否当前页面打开',
+			alias: ['s'],
+			type: 'boolean',
 			defaultValue: false,
 		},
 		{
-			key: "picture",
-			desc: "是否搜索图片",
-			alias: ["p"],
-			type: "boolean",
+			key: 'picture',
+			desc: '是否搜索图片',
+			alias: ['p'],
+			type: 'boolean',
 			defaultValue: false,
 		},
 	],
 	action(options, terminal) {
-		const { _, self, picture } = options;
-		const word = _.length > 0 ? _.join(" ") : "";
-		let targetLink = `https://www.baidu.com/s?wd=${word?.trim()}`;
+		const { _, self, picture } = options
+		const word = _.length > 0 ? _.join(' ') : ''
+		let targetLink = `https://www.baidu.com/s?wd=${word?.trim()}`
 		// 搜索图片
 		if (picture) {
-			targetLink = `https://image.baidu.com/search/index?tn=baiduimage&word=${word?.trim()}`;
+			targetLink = `https://image.baidu.com/search/index?tn=baiduimage&word=${word?.trim()}`
 		}
 		if (self) {
-			window.location.href = targetLink;
+			window.location.href = targetLink
 		} else {
-			window.open(targetLink);
+			window.open(targetLink)
 		}
 	},
-};
+}
 
-export default baiduCommand;
+export default baiduCommand
